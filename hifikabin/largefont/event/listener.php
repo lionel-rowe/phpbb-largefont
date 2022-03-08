5b7e0c48448085aadeaa9f0d371ba7586820c6cb
lionel-rowe <lionel.rowe@gmail.com>
2022-03-08 02:38:24 +0000

    Fix HTML nesting bug with proportional font scaling

--- hifikabin/largefont/event/listener.php
+++ hifikabin/largefont/event/listener.php
@@ -18,23 +18,42 @@ class listener implements EventSubscriberInterface
 {
 	/** @var \phpbb\template\template */
 	protected $template;
-	
+
 	/** @var \phpbb\user */
 	protected $user;
-	
+
+	/**
+	 * @var array $size_map
+	 *
+	 * maps the internally-used legacy values back to
+	 * the original values specified by the user
+	 */
+	protected $size_map;
+
 	public function __construct(\phpbb\template\template $template, \phpbb\user $user)
 	{
 		$this->template = $template;
 		$this->user = $user;
+
+		$this->size_map = array(
+			101 => 105,
+			102 => 110,
+			103 => 115,
+			104 => 130,
+			106 => 150,
+			110 => 200,
+			114 => 300,
+			120 => 400,
+		);
 	}
-	
+
 	static public function getSubscribedEvents()
 	{
 		return array(
 			'core.page_header' => 'main',
 		);
 	}
-	
+
 	public function main($event)
 	{
 		$this->template->assign_vars(array(
@@ -43,6 +62,9 @@ class listener implements EventSubscriberInterface
 			'S_USER_FONT_SIZE'			=> $this->user->data['user_font_size'],
 			'S_USER_MOBILE_FONT'		=> $this->user->data['user_mobile_font'],
 			'S_USER_MOBILE_FONT_SIZE'	=> $this->user->data['user_mobile_font_size'],
+
+			'S_REAL_FONT_SIZE'          => $this->size_map[$this->user->data['user_font_size']],
+			'S_REAL_MOBILE_FONT_SIZE'   => $this->size_map[$this->user->data['user_mobile_font_size']],
 		));
 	}
 }
